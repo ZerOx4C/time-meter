17bcfba4823e8a6176da0f8c3138a85a4a4b933d
ZerOx4C <zerox4c@gmail.com>
2024-03-10 23:26:38 +0900

    色を正しく解釈できていなかったのを修正した

--- setting/color_hex_string.go
+++ setting/color_hex_string.go
@@ -10,7 +10,13 @@ import (
 type colorHexString winapi.COLORREF
 
 func (crw *colorHexString) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf("#%06x", *crw)), nil
+	var colorRef winapi.COLORREF = winapi.COLORREF(*crw)
+	r := winapi.GetRValue(colorRef)
+	g := winapi.GetGValue(colorRef)
+	b := winapi.GetBValue(colorRef)
+
+	str := fmt.Sprintf("#%02x%02x%02x", r, g, b)
+	return []byte(str), nil
 }
 
 func (crw *colorHexString) UnmarshalJSON(data []byte) error {
@@ -19,6 +25,11 @@ func (crw *colorHexString) UnmarshalJSON(data []byte) error {
 		return err
 	}
 
-	_, err := fmt.Sscanf(str, "#%06x", crw)
-	return err
+	var r, g, b int32
+	if _, err := fmt.Sscanf(str, "#%02x%02x%02x", &r, &g, &b); err != nil {
+		return err
+	}
+
+	*crw = colorHexString(winapi.RGB(r, g, b))
+	return nil
 }
